7061e405b370a290af7e6868045275537f407b1a
Bruno Windels <bruno.windels@gmail.com>
2016-03-09 13:29:45 +0100

    clear and write to term in main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod input;
 mod output;
+mod geom;
 
 use input::PollResult;
 use std::time::Duration;
+use output::ansiterm::AnsiTerm;
+use geom::Point;
 
 fn main() {
-	let term = output::ansiterm::AnsiTerm::from_stdout();
+	let mut term = AnsiTerm::from_stdout().unwrap();
 	term.clear();
+	let message = format!("the terminal has {} rows and {} columns", term.rows(), term.columns());
+	term.write(Point {x: 5, y: 5}, message.as_str());
 
 	let mut keyboard = input::create_keyboard_poller().unwrap();
 	match keyboard.poll(Duration::from_millis(1000)) {
