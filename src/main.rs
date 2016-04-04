8b98b1006703f364c6ee028b6c2dccfb873e439b
Bruno Windels <bruno.windels@gmail.com>
2016-04-05 00:13:12 +0200

    move app to own file

--- src/main.rs
+++ src/main.rs
@@ -2,69 +2,14 @@ mod input;
 mod output;
 mod geom;
 mod time;
+mod app;
 
-use std::time::Duration;
-use input::{Keyboard, PollResult, Key};
+use app::SnakeApp;
+use input::Keyboard;
 use output::Terminal;
 use output::ansiterm::AnsiTerm;
+use std::time::Duration;
 use time::Timer;
-use geom::Point;
-
-struct SnakeApp {
-	keyboard: Box<Keyboard>,
-	term: Box<Terminal>,
-	timer: Box<Timer>,
-	interval: Duration,
-}
-
-impl SnakeApp {
-
-	pub fn run(&mut self) {
-		self.term.clear();
-
-		let size = self.term.size();
-		let message = format!("the screen has height {} and width {}", size.height, size.width);
-		self.term.write(Point {x: 5, y: 5}, message.as_str());
-		self.term.write(Point {x: 5, y: 10}, "Watching keyboard...");
-
-		let mut should_exit = false;
-		
-		while !should_exit {
-			should_exit = self.wait_for_keypress();
-		}
-
-		self.term.write(Point {x:0, y: 15}, "");
-	}
-
-	fn wait_for_keypress(&mut self) -> bool {
-		let mut should_exit = false;
-		let start = self.timer.now();
-		let pos2 = Point {x: 5, y: 11};
-
-		match self.keyboard.poll(self.interval) {
-			PollResult::Timeout => {
-				self.term.write(pos2, "timed out");
-			},
-			PollResult::Err(msg) => {
-				panic!("error: {:?}", msg);
-			}
-			PollResult::KeyPressed(key) => {
-				should_exit = match key {
-					Key::Esc => true,
-					_ => false,
-				};
-				self.term.write_repeated(pos2, " ", 40);
-				self.term.write(pos2, format!("key pressed {}", key).as_str())
-			},
-		};
-		let end = self.timer.now();
-		let elapsed = self.timer.diff(start, end);
-		self.term.write(Point {x: 40, y: 11}, format!("elapsed time: {:?}", elapsed).as_str());
-
-		should_exit
-	}
-}
-
 
 fn main() {
 
