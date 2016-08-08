1ef11c381f834833b74e31c36ef2eb79f608b6ba
Bruno Windels <bruno.windels@gmail.com>
2016-08-08 11:42:40 +0200

    fix compiler errors in snake

--- src/game/snake.rs
+++ src/game/snake.rs
@@ -9,28 +9,28 @@ pub struct Snake {
 
 impl Snake {
 
-  pub fn new(s: Segment) {
+  pub fn new(s: Segment) -> Snake {
     let segments = vec!(s);
     Snake {segments: segments}
   }
 
-  pub fn head(&self) {
-    self.segments.last().unwrap().head();
+  pub fn head(&self) -> Point {
+    self.segments.last().unwrap().head()
   }
   
   pub fn contains(&self, p: Point) -> bool {
-    self.segments.any(|s: Segment| s.contains(p))
+    self.segments.iter().any(|s: &Segment| s.contains(p))
   }
 
   pub fn draw<S: Screen>(&self, screen: &mut S) {
-    for s in self.segments {
-      screen.draw_segment(s, Symbol::SnakeBody);
+    for s in &self.segments {
+      screen.draw_segment(*s, Symbol::SnakeBody);
     }
   }
 
   pub fn shrink_tail(&mut self)  {
     let tail_segment = self.segments[0].shrink_tail().unwrap();
-    if tail_segment.is_empty() && self.segments.size() > 1 {
+    if tail_segment.is_empty() && self.segments.len() > 1 {
       self.segments.remove(0);
     }
     else {
@@ -38,23 +38,24 @@ impl Snake {
     }
   }
 
-  pub fn grow_head(&mut self, dir: RelativeDirection, amount: UCoordinate) {
-    let head_segment = self.segments.last().unwrap();
+  pub fn grow_head(&mut self, dir: RelativeDirection) {
+  	let segments = &mut self.segments;
+    let head_segment = *segments.last().unwrap();
 
     let new_direction = match dir {
-      Left => Some(head_segment.direction.turn_ccw()),
-      Right => Some(head_segment.direction.turn_cw()),
-      Straight => None
+      RelativeDirection::Left => Some(head_segment.direction().turn_ccw()),
+      RelativeDirection::Right => Some(head_segment.direction().turn_cw()),
+      RelativeDirection::Straight => None
     };
 
     match new_direction {
       Some(dir) => {
         let tail = head_segment.head() + dir.to_point();
-        let new_head_segment = Segment::new(tail, dir, amount);
-        self.segments.push(new_head_segment);
+        let new_head_segment = Segment::new(tail, dir, 1);
+        segments.push(new_head_segment);
       },
       None => {
-        head_segment = head_segment.grow_head(amount);
+        *segments.last_mut().unwrap() = head_segment.grow_head();
       }
     };
   }
