da690851538857d09a97abc5a3a370fa9f66ab8e
Bruno Windels <bruno.windels@gmail.com>
2016-08-09 16:39:47 +0200

    game over when colliding with wall

--- src/game/snake/game.rs
+++ src/game/snake/game.rs
@@ -1,11 +1,11 @@
 use output::Screen;
 use game::Game;
-use geom::Size;
+use geom::{Size, Point};
 use input::Key;
 use std::time::Duration;
 use super::level::Level;
 use super::field::Field;
-use super::RelativeDirection;
+use super::{RelativeDirection, StepResult};
 
 fn key_to_relative_direction(key_option: Option<Key>) -> RelativeDirection {
   match key_option {
@@ -19,7 +19,8 @@ fn key_to_relative_direction(key_option: Option<Key>) -> RelativeDirection {
 }
 
 pub struct SnakeGame {
-  level: Level
+  level: Level,
+  game_over: bool
 }
 
 impl SnakeGame {
@@ -29,7 +30,8 @@ impl SnakeGame {
     let level = Level::new(field);
 
     SnakeGame {
-      level: level
+      level: level,
+      game_over: false
     }
   }
 
@@ -38,17 +40,37 @@ impl SnakeGame {
 impl Game for SnakeGame {
 
   fn update(&mut self, input: Option<Key>, passed_time: Duration) -> bool {
+
+    if self.game_over {
+      return false;
+    }
+
     let rel_dir = key_to_relative_direction(input);
-    self.level.step(rel_dir);
-    true
+    let step_result = self.level.step(rel_dir);
+
+    match step_result {
+      StepResult::Collision => {self.game_over = true;},
+      _ => {}
+    };
+
+    return true;
   }
 
   fn max_idle_time(&self) -> Option<Duration> {
-    Some(Duration::from_millis(200))
+    if self.game_over {
+      None
+    }
+    else {
+      Some(Duration::from_millis(200))
+    }
   }
 
   fn initial_draw<S: Screen>(&self, screen: &mut S) {
     self.level.full_draw(screen);
+
+    if self.game_over {
+      screen.draw_text(Point::new(8, 5), "GAME OVER! HIT ESC TO LEAVE THE GAME");
+    }
   }
 
 }
