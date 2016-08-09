0f79c76d37e9333fc63858fded4006bdc329b711
Bruno Windels <bruno.windels@gmail.com>
2016-08-09 11:17:56 +0200

    make polling code less dense

--- src/app.rs
+++ src/app.rs
@@ -42,13 +42,16 @@ impl<A: Keyboard, B: Screen, C: Timer> SnakeApp<A, B, C> {
 
   fn wait_for_keypress(&mut self) -> Option<Key> {
     let start = self.timer.now();
-    let mut key : Option<Key> = None;
+    let mut key = None;
     let mut passed_time = Duration::from_millis(0);
     let mut return_immediately = false;
 
     while !return_immediately && passed_time < self.interval {
 
-      match self.keyboard.poll(self.interval - passed_time) {
+      let remaining_time = self.interval - passed_time;
+      let result = self.keyboard.poll(remaining_time); 
+
+      match result {
         PollResult::KeyPressed(k) => {
           key = Some(k);
           return_immediately = match k {
